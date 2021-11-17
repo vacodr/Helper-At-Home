f812250f818d4a52a6cd07f7b1231963e4d76b62
vacodr <vaibhavcool614@gmail.com>
2021-11-17 16:24:23 +0530

    added build version

---
+++ client/build/precache-manifest.d6c11ca1179bf659b57156cffa36c2ee.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1509f676cecf81acefcecd62cf782762",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f62a87c23df45d97115",
+    "url": "/static/css/2.f47c5051.chunk.css"
+  },
+  {
+    "revision": "2825530fe91155f9800f",
+    "url": "/static/css/main.a2df1264.chunk.css"
+  },
+  {
+    "revision": "8f62a87c23df45d97115",
+    "url": "/static/js/2.63ab5208.chunk.js"
+  },
+  {
+    "revision": "cc5370d0a132a09d2cc36e018aeaf61d",
+    "url": "/static/js/2.63ab5208.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2825530fe91155f9800f",
+    "url": "/static/js/main.799edb3c.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "6508d4d9e91d8916099c5eee4aa920b8",
+    "url": "/static/media/elec.6508d4d9.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "6d5ca99dad65f23688ada7c2c758e06d",
+    "url": "/static/media/paint.6d5ca99d.jpg"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "869fcc32c231a66f6a5fb15f1a498500",
+    "url": "/static/media/weld.869fcc32.jpg"
+  }
+]);
